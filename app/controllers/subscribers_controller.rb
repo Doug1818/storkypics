f719acec2932ecfab49f82bc1a3ba0f4e19d79c0
Doug Raicek <doug.raicek@gmail.com>
2013-03-04 22:47:12 -0500

    Fix mailchimp exception handling

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,16 +2,18 @@ class SubscribersController < ApplicationController
   
   def create
     @subscriber = Subscriber.new(params[:subscriber])
-    if @subscriber.save
-      flash[:success] = "Thanks for your interest in Storkypics!"
-      redirect_to root_path
+    begin
       gb = Gibbon.new(ENV['MC_API_KEY'])
-      gb.throws_exceptions = false
       list_id = gb.lists({:list_name => "Launch List"})["data"].first["id"]
       gb.list_subscribe(:id => list_id, :email_address => @subscriber.email, :merge_vars => {'fname' => @subscriber.first_name, 'lname' => @subscriber.last_name }, :double_optin => false)
-    else
-      flash[:error] = "Please enter a valid name and email"
+    rescue Gibbon::MailChimpError => e
+      logger.error "Mailchimp error while retrieving lists: #{e.message}"
+      flash[:error] = "We're sorry, an error occurred. Either your information is incorrect or you have already signed up."
       redirect_to root_path
+      return
     end
+    @subscriber.save
+    flash[:success] = "Thank you for your interest in Storkypics!"
+    redirect_to root_path
   end
 end
